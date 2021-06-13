23d1fe05cd4bb698692a44f705c0a6c4b51b276d
Przemek Bochman <przemyslaw.bochman@gmail.com>
2021-06-13 14:51:56 +0200

    Deleted console.log

--- js/script.js
+++ js/script.js
@@ -1,48 +1,27 @@
 'use strict';
-console.log('Initiation');
 
 function titleClickHandler(event){
     event.preventDefault();
     const clickedElement = this;
-    console.log('Link was clicked!');
-     console.log(event);
 
     const activeLinks = document.querySelectorAll('.titles a.active');
   
-  for(let activeLink of activeLinks){
-    activeLink.classList.remove('active');
-  }
+    for(let activeLink of activeLinks){
+        activeLink.classList.remove('active');
+    }
   
-  console.log('clickedElement:', clickedElement);
-
-  const activeArticles = document.querySelectorAll('.posts article.active');
+    const activeArticles = document.querySelectorAll('.posts article.active');
   
-  for(let activeArticle of activeArticles){
-    activeArticle.classList.remove('active');
-  }
+    for(let activeArticle of activeArticles){
+         activeArticle.classList.remove('active');
+    }
 
-clickedElement.classList.add('active');
-
-const articleSelector = clickedElement.getAttribute("href");
-console.log("wartość articleSelector to: " + articleSelector);
-  
-
-const articleId = articleSelector.replace('#','');
-console.log("wartość POPRAWIONA articleId to: " + articleId);
-
- 
- 
-const targetArticle = document.querySelector(`[id='${articleId}']`);
-targetArticle.classList.add('active');
-
-  
-  
-    
+    clickedElement.classList.add('active');
+    const articleSelector = clickedElement.getAttribute("href");
+    const articleId = articleSelector.replace('#','');
+    const targetArticle = document.querySelector(`[id='${articleId}']`);
+    targetArticle.classList.add('active');   
 }
-
-
-
-
 
 const links = document.querySelectorAll('.titles a');
 
