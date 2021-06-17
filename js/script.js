26b345721eef5e72a1ef479250c5c6a003ea5aa0
Przemek Bochman <przemyslaw.bochman@gmail.com>
2021-06-17 17:21:14 +0200

    Beta without tag cloud

--- js/script.js
+++ js/script.js
@@ -2,13 +2,10 @@
 
 //SECOND MODULE
 
-//const optArticleSelector = '.post';
 const  optTitleSelector = '.post-title';
-//const  optTitleListSelector = '.titles';
 const optArticleTagsSelector = '.post-tags .list';
 
 function generateTitleLinks(){
-    //const titleList = (document.querySelector(optTitleListSelector)).innerHTML = '';
     const articles = document.querySelectorAll('.post');
     let html = '';
 
@@ -21,21 +18,44 @@ function generateTitleLinks(){
     let listInsertPoint = document.getElementById('link-title-list');
     listInsertPoint.insertAdjacentHTML('afterbegin',html);
 }
-generateTitleLinks();
+generateTitleLinks(); //END OF SECOND MODULE
+
+//THIRD MODULE generating tags
+
+function generateTags(){
+    let html = '';
+    const articles = document.querySelectorAll('.post.active');
+   
+   
+    for(let article of articles){
+        const articleTags = article.getAttribute('data-tags');
+        const articleTagsArray = articleTags.split(' ');
+        //console.log(articleTagsArray);        
+
+        for (let tag of articleTagsArray)
+        {
+            const tagHTML = '<li><a href="#tag-' + tag +'"><span>' + tag +'</span></a></li>';  
+            html = html + tagHTML;
+         }  
+    } 
+    let tagInsertPoint = document.getElementById('tag-list');
+    tagInsertPoint.insertAdjacentHTML('beforeend',html);
+  }
+  generateTags();   //END OF THIRD MODULE
 
 
 // FIRST MODULE, adding classes, opening articles
 function titleClickHandler(event){
     event.preventDefault();
     const clickedElement = this;
-
     const activeLinks = document.querySelectorAll('.titles a.active');
   
-    for(let activeLink of activeLinks){ // delete active
+    for(let activeLink of activeLinks) { 
         activeLink.classList.remove('active');
     }
   
     const activeArticles = document.querySelectorAll('.posts article.active');
+
   
     for(let activeArticle of activeArticles){
         activeArticle.classList.remove('active');
@@ -46,35 +66,47 @@ function titleClickHandler(event){
     const articleId = articleSelector.replace('#','');
     const targetArticle = document.querySelector(`[id='${articleId}']`);
     targetArticle.classList.add('active');   
+    generateTags();// NIE KASUJĄSIĘSTARE TAGI!!
 }
-
 const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
     link.addEventListener('click', titleClickHandler);
 }    
 
+function tagClickHandler(event) {
+    event.preventDefault();         /* prevent default action for this event */
+    const clickedElement = this;    // make new constant named "clickedElement" and give it the value of "this" */
+    const href = clickedElement.getAttribute('href');   // make a new constant "href" and read the attribute "href" of the clicked element
+    const tag = href;  /* make a new constant "tag" and extract tag from the "href" constant */
+    const tagLinks = document.querySelectorAll('.tag-links .active'); /* find all tag links with class active */
+    //console.log('wyświetlanie TAG-LINKS',tagLinks);
 
-//THIRD MODULE generating tags
+    for(tag of tagLinks) { 
+        tagLinks.remove('active');
+    }
 
-function generateTags(){
-    const articles = document.querySelectorAll('.post');
-    let html = '';
+    //document.querySelector('a[href='${href}']');  //TUTAJ zostało zrobić porównanieXXXXXXXXXXXXXXXXXXXXXXXXX
+    //$('a[href=',a_href)
+  // find all tag links with "href" attribute equal to the "href" constant */
 
-    for(let article of articles){
-        const articleTags = article.getAttribute('data-tags');
-        const articleTagsArray = articleTags.split(' ');
-        console.log(articleTagsArray);
-        
-        for (let tag of articleTagsArray)
-        {
-            const tagHTML = '<li><a href="#tag-' + tag +'"><span>' + tag +'</span></a></li>';  
-            html = html + tagHTML;
-        }  
-        
-    } 
-    console.log(html);
-    let tagInsertPoint = document.getElementById('tag-list');
-    tagInsertPoint.insertAdjacentHTML('beforeend',html);
+  /* START LOOP: for each found tag link */
+
+    /* add class active */
+
+  /* END LOOP: for each found tag link */
+
+  /* execute function "generateTitleLinks" with article selector as argument */ 
+}
+
+function addClickListenersToTags(){
+    /* find all links to tags */
+    
+    /* START LOOP: for each link */
+  
+      /* add tagClickHandler as event listener for that link */
+  
+    /* END LOOP: for each link */
   }
-  generateTags();
+  
+  addClickListenersToTags();
