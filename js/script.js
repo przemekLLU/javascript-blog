e6eff60d9cefde4800eb3bdbd8aae28259e644b9
Przemek Bochman <przemyslaw.bochman@gmail.com>
2021-07-15 17:21:19 +0200

    tag cloud, almost

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 'use strict';
 
-//SECOND MODULE
+
 
 const  optTitleSelector = '.post-title';
 const optArticleSelector = '.post';
-const optTagsListSelector = '.tags.list';
-//const optArticleTagsSelector = '.post-tags .list';
+//const optTagsListSelector = '.tags.list';
 
 function generateTitleLinks(customSelector = ''){
     const articles = document.querySelectorAll(optArticleSelector + customSelector);
@@ -20,21 +19,23 @@ function generateTitleLinks(customSelector = ''){
     let listInsertPoint = document.getElementById('link-title-list');
     listInsertPoint.insertAdjacentHTML('afterbegin',html);
 }
-generateTitleLinks(); //END OF SECOND MODULE
+generateTitleLinks(); 
 
-//THIRD MODULE generating tags
+
 
 function generateTags() {
     const articles = document.querySelectorAll('.post');
     let allTags = {};
     let allTagsHTML ='';
+    let tagHTML = '';
+    let tag;
     
+
     for(let article of articles) {
         let html = '';
-        let tagHTML ='';
         const articleTags = article.getAttribute('data-tags');
         const articleTagsArray = articleTags.split(' ');
-        for (let tag of articleTagsArray) {
+        for (tag of articleTagsArray) {
             tagHTML = '<li><a class="tag-link active" href="#tag-' + tag +'"><span>' + tag +'</span></a></li>'+ ' ';
             html = html + tagHTML;
             // eslint-disable-next-line no-prototype-builtins
@@ -43,21 +44,18 @@ function generateTags() {
             }   else    {
                 allTags[tag]++;
             }
-            allTagsHTML += tagHTML + ' (' + allTags[tag] + ')';
+        
         }
         const tagsElement = article.querySelector('.list.list-horizontal');
         tagsElement.innerHTML = html;
+        allTagsHTML += tagHTML + ' (' + allTags[tag] + ')';
     }    
-    console.log('allTagsHTML', allTagsHTML);
-    
-
     const tagList = document.querySelector('.tags');
-    tagList.innerHTML = allTagsHTML;
-    
+    tagList.innerHTML = allTagsHTML;   
 }
-generateTags();   //END OF THIRD MODULE
+generateTags();   
 
-//BEGIN OF FOURTH MODULE
+
 
 function generateAuthor()   {
     const articles = document.querySelectorAll(optArticleSelector);
@@ -75,9 +73,6 @@ function generateAuthor()   {
 generateAuthor();
 
 
-//END OF FOURTH MODULE
-
-// FIRST MODULE, adding classes, opening articles
 function titleClickHandler(event)   {
     event.preventDefault();
     const clickedElement = this;
@@ -113,9 +108,9 @@ function tagClickHandler(event) {
 
     for (let article of articles) {
         if (article.getAttribute('data-tags').includes(tag_class)) {
-            article.classList.add('active')
+            article.classList.add('active');
         } else {
-            article.classList.remove('active')
+            article.classList.remove('active');
         }
     }
 }
